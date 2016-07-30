790da9c5937efe3022dbd357216187c7390e8086
nishanth home <nishanth home@Nishanth>
2016-07-29 17:18:09 -0700

    Third Commit

--- CodingTest/src/CodingTest/AnonymousArray.java
+++ CodingTest/src/CodingTest/AnonymousArray.java
@@ -7,7 +7,10 @@ public class AnonymousArray {
 	 public static void main(String[] args) {
 	        System.out.println("Sum of numbers: " + sum(new int[]{6,3,55,3,55,22,54}));
 	    }
-	 
+	 //Anonymous array follows same syntax like normal array in Java e.g. new [] { };
+
+
+
 	    public static int sum(int[] numbers){
 	        int Sum = 0;
 	        for(int x: numbers){
