3f1001738a4ae077e6c7b1504b573b60fb3200ec
nishanth home <nishanth home@Nishanth>
2016-07-29 17:11:00 -0700

    Second Commit

--- CodingTest/src/CodingTest/AnonymousArray.java
+++ CodingTest/src/CodingTest/AnonymousArray.java
@@ -1,10 +1,13 @@
 package CodingTest;
 
+//Anonymous array is an array without any name.
+//Second Commit
 public class AnonymousArray {
 
 	 public static void main(String[] args) {
 	        System.out.println("Sum of numbers: " + sum(new int[]{6,3,55,3,55,22,54}));
 	    }
+	 
 	    public static int sum(int[] numbers){
 	        int Sum = 0;
 	        for(int x: numbers){
